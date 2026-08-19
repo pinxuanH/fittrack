4b32d60034b67dc50f1fd4b1317fb291ee2f6ab6
pinxuanH <46783447+pinxuanH@users.noreply.github.com>
2026-08-19 18:29:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* FitTrack service worker:離線快取 App 外殼 */
-const CACHE = "fittrack-v11";
+const CACHE = "fittrack-v12";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest",
                 "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
